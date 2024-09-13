48fb5ca9f02b6e4ffa4bae76c9efb4fa1ecd2315
José Brito <josealbritose@gmail.com>
2024-09-12 21:24:09 -0300

    DarkModeBottom

--- src/app/components/shared/Header.tsx
+++ src/app/components/shared/Header.tsx
@@ -3,6 +3,7 @@ import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkModeSwitch from "../ui/DarkModeSwitch";
 
 export default function Header() {
   return (
@@ -16,12 +17,13 @@ export default function Header() {
           size={24}
         />
       </nav>
-      <div className="flex gap-1 items-center">
+      <div className="flex flex-col gap-1 items-center">
+      <DarkModeSwitch/>
         <Link href={"/"}>
           <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
             IMDb
           </span>
-          <span>Clone</span>
+          <span className="font-semibold text-xl text-center px-1">Clone</span>
         </Link>
       </div>
     </header>
