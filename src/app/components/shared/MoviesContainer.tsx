8abf41a387e472c6ddb9929b5d0b1e700c4b0611
José Brito <josealbritose@gmail.com>
2024-09-17 09:15:06 -0300

    MovieContainer Component

---
+++ src/app/components/shared/MoviesContainer.tsx
@@ -0,0 +1,18 @@
+import { Movie } from "@/app/interfaces/MoviesInterface";
+import React from "react";
+import MovieCard from "./MovieCard";
+
+interface MoviesProps {
+  movies: Movie[];
+}
+
+export default function Movies({ movies }: MoviesProps) {
+  console.log(movies[0])
+  return (
+    <section className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto py-4">
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id} />
+      ))}
+    </section>
+  );
+}
