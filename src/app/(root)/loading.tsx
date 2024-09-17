aab5f89efc3172a5b39c97d4d68d1df21c44e642
José Brito <josealbritose@gmail.com>
2024-09-17 10:18:54 -0300

    Loading spinner

---
+++ src/app/(root)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import spinner from '../../../public/spinner.svg'
+import Image from 'next/image'
+export default function loading() {
+  return (
+    <div className='flex justify-center'>
+      <Image src={spinner} alt="loading..." height={36} width={36} fill />
+    </div>
+  )
+}
